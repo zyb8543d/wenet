ff98be2ecee8247db8b9fe90be0a0d76fec3341e
zyb8543 <38179632+zyb8543d@users.noreply.github.com>
2026-04-03 18:47:06 +0800

    Create MainActivity_back.java

---
+++ runtime/android/app/src/main/java/com/mobvoi/wenet/MainActivity_back.java
@@ -0,0 +1,303 @@
+package com.mobvoi.wenet;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Process;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+public class MainActivity extends AppCompatActivity {
+
+  private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
+  private static final String LOG_TAG = "WENET";
+  private static final int SAMPLE_RATE = 16000;  // The sampling rate
+  private static final int MAX_QUEUE_SIZE = 2500;  // 100 seconds audio, 1 / 0.04 * 100
+  private static final List<String> resource = Arrays.asList(
+    "final.zip", "units.txt", "ctc.dla", "decoder.dla", "encoder.dla", "embedding_pos.bin","embedding_token.bin"
+  );
+
+  private boolean startRecord = false;
+  private AudioRecord record = null;
+  private int miniBufferSize = 0;  // 1280 bytes 648 byte 40ms, 0.04s
+  private final BlockingQueue<short[]> bufferQueue = new ArrayBlockingQueue<>(MAX_QUEUE_SIZE);
+
+
+  public static void assetsInit(Context context) throws IOException {
+    AssetManager assetMgr = context.getAssets();
+    // Unzip all files in resource from assets to context.
+    // Note: Uninstall the APP will remove the resource files in the context.
+    for (String file : assetMgr.list("")) {
+      if (resource.contains(file)) {
+        File dst = new File(context.getFilesDir(), file);
+        if (!dst.exists() || dst.length() == 0) {
+          Log.i(LOG_TAG, "Unzipping " + file + " to " + dst.getAbsolutePath());
+          InputStream is = assetMgr.open(file);
+          OutputStream os = new FileOutputStream(dst);
+          byte[] buffer = new byte[4 * 1024];
+          int read;
+          while ((read = is.read(buffer)) != -1) {
+            os.write(buffer, 0, read);
+          }
+          os.flush();
+        }
+      }
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode,
+      String[] permissions, int[] grantResults) {
+    if (requestCode == MY_PERMISSIONS_RECORD_AUDIO) {
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        Log.i(LOG_TAG, "record permission is granted");
+        initRecorder();
+      } else {
+        Toast.makeText(this, "Permissions denied to record audio", Toast.LENGTH_LONG).show();
+        Button button = findViewById(R.id.button);
+        button.setEnabled(false);
+      }
+    }
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    // adb shell pm grant com.mobvoi.wenet android.permission.RECORD_AUDIO
+    requestAudioPermissions();
+/*    try {
+      assetsInit(this);
+    } catch (IOException e) {
+      Log.e(LOG_TAG, "Error process asset files to file path");
+    }*/
+
+    TextView textView = findViewById(R.id.textView);
+    textView.setText("");
+    Recognize.init(getFilesDir().getPath());
+    //Recognize.init("/data/local/tmp/resource"/*getFilesDir().getPath()*/);
+
+    Button button = findViewById(R.id.button);
+    button.setText("Start Record");
+    button.setOnClickListener(view -> {
+      if (!startRecord) {
+        startRecord = true;
+        Recognize.reset();
+        startRecordThread();
+        startAsrThread();
+        Recognize.startDecode();
+        button.setText("Stop Record");
+      } else {
+        startRecord = false;
+        Recognize.setInputFinished();
+        button.setText("Start Record");
+      }
+      button.setEnabled(false);
+    });
+  }
+
+  private void requestAudioPermissions() {
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+        != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(this,
+          new String[]{Manifest.permission.RECORD_AUDIO},
+          MY_PERMISSIONS_RECORD_AUDIO);
+    } else {
+      initRecorder();
+    }
+  }
+
+  private void initRecorder() {
+    // buffer size in bytes 1280
+    miniBufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE,
+        AudioFormat.CHANNEL_IN_MONO,
+        AudioFormat.ENCODING_PCM_16BIT)*2;
+    if (miniBufferSize == AudioRecord.ERROR || miniBufferSize == AudioRecord.ERROR_BAD_VALUE) {
+      Log.e(LOG_TAG, "Audio buffer can't initialize!");
+      return;
+    }
+    record = new AudioRecord(MediaRecorder.AudioSource.DEFAULT,
+    //record = new AudioRecord(MediaRecorder.AudioSource.MIC,
+        SAMPLE_RATE,
+        AudioFormat.CHANNEL_IN_MONO,
+        AudioFormat.ENCODING_PCM_16BIT,
+        miniBufferSize);
+    if (record.getState() != AudioRecord.STATE_INITIALIZED) {
+      Log.e(LOG_TAG, "Audio Record can't initialize!");
+      return;
+    }
+    Log.i(LOG_TAG, "Record init okay");
+  }
+  private short[] getChannelData(short[] data, int channel_index) {
+    if (data == null || data.length <= 0) {
+      return null;
+    }
+    short[] result = new short[data.length / 2];
+    for (int index = 0; index < data.length / 2;index++) {
+      result[index] =  data[index*2 + channel_index];
+    }
+    return result;
+  }
+
+
+  private void startRecordThread() {
+    new Thread(() -> {
+      VoiceRectView voiceView = findViewById(R.id.voiceRectView);
+      record.startRecording();
+      Process.setThreadPriority(Process.THREAD_PRIORITY_AUDIO);
+      while (startRecord) {
+        short[] buffer = new short[miniBufferSize / 2];
+        int read = record.read(buffer, 0, buffer.length);
+        voiceView.add(calculateDb(buffer));
+        try {
+          if (AudioRecord.ERROR_INVALID_OPERATION != read) {
+            bufferQueue.put(buffer);
+            //bufferQueue.put(getChannelData(buffer, 0));
+          }
+        } catch (InterruptedException e) {
+          Log.e(LOG_TAG, e.getMessage());
+        }
+        Button button = findViewById(R.id.button);
+        if (!button.isEnabled() && startRecord) {
+          runOnUiThread(() -> button.setEnabled(true));
+        }
+      }
+      record.stop();
+      voiceView.zero();
+    }).start();
+  }
+
+//private void startRecordThread() {
+//  new Thread(() -> {
+//    VoiceRectView voiceView = findViewById(R.id.voiceRectView);
+//    record.startRecording();
+//    Process.setThreadPriority(Process.THREAD_PRIORITY_AUDIO);
+//
+//    // 0.创建音频文件，音频文件名称为当前时间戳.pcm  单通道 16k  --------
+//    String fileName = "audio_" + System.currentTimeMillis() + ".pcm";
+//    File audioFile = new File("/data/local/tmp/resource", fileName);
+//    FileOutputStream fos = null;
+//    try{
+//      fos = new FileOutputStream(audioFile);
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//    }
+//    //------------------------------------------------------------
+//
+//    while (startRecord) {
+//      short[] buffer = new short[miniBufferSize / 2];
+//      int read = record.read(buffer, 0, buffer.length);
+//
+//      // 1. 将buffer写入音频文件 -------------------------------
+//      if (read > 0) {
+//        // 将short数组转换为byte数组
+//        byte[] byteBuffer = new byte[read * 2]; // 每个short占2个字节
+//        for (int i = 0; i < read; i++) {
+//          byteBuffer[2 * i] = (byte) (buffer[i] & 0xff);
+//          byteBuffer[2 * i + 1] = (byte) ((buffer[i] >> 8) & 0xff);
+//        }
+//        try{
+//          fos.write(byteBuffer, 0, byteBuffer.length);
+//        } catch (Exception e) {
+//          e.printStackTrace();
+//        }
+//
+//      }
+//      //------------------------------------------------------------
+//
+//      voiceView.add(calculateDb(buffer));
+//      try {
+//        if (AudioRecord.ERROR_INVALID_OPERATION != read) {
+////            bufferQueue.put(getChannelData(buffer, 0));
+//          bufferQueue.put(buffer);
+//        }
+//      } catch (InterruptedException e) {
+//        Log.e(LOG_TAG, e.getMessage());
+//      }
+//      Button button = findViewById(R.id.button);
+//      if (!button.isEnabled() && startRecord) {
+//        runOnUiThread(() -> button.setEnabled(true));
+//      }
+//    }
+//    // 2.保存音频 ------------------------------------------------
+//    if (fos != null) {
+//      try {
+//        fos.close();
+//      } catch (IOException e) {
+//        e.printStackTrace();
+//      }
+//    }
+//    //------------------------------------------------------------
+//    record.stop();
+//    voiceView.zero();
+//  }).start();
+//}
+
+  private double calculateDb(short[] buffer) {
+    double energy = 0.0;
+    for (short value : buffer) {
+      energy += value * value;
+    }
+    energy /= buffer.length;
+    energy = (10 * Math.log10(1 + energy)) / 100;
+    energy = Math.min(energy, 1.0);
+    return energy;
+  }
+
+  private void startAsrThread() {
+    new Thread(() -> {
+      // Send all data
+      while (startRecord || bufferQueue.size() > 0) {
+        try {
+          short[] data = bufferQueue.take();
+          // 1. add data to C++ interface
+          Recognize.acceptWaveform(data);
+          // 2. get partial result
+          runOnUiThread(() -> {
+            TextView textView = findViewById(R.id.textView);
+            textView.setText(Recognize.getResult());
+          });
+        } catch (InterruptedException e) {
+          Log.e(LOG_TAG, e.getMessage());
+        }
+      }
+
+      // Wait for final result
+      while (true) {
+        // get result
+        if (!Recognize.getFinished()) {
+          runOnUiThread(() -> {
+            TextView textView = findViewById(R.id.textView);
+            textView.setText(Recognize.getResult());
+          });
+        } else {
+          runOnUiThread(() -> {
+            Button button = findViewById(R.id.button);
+            button.setEnabled(true);
+          });
+          break;
+        }
+      }
+    }).start();
+  }
+
+
+}
